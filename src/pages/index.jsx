467a95a5d7e3c37d6bcc4b9acf138c6b033c440f
jadewisemann <jadewisemann@gmail.com>
2025-03-05 22:17:57 +0900

    feat: add basic routing

---
+++ src/pages/index.jsx
@@ -0,0 +1,23 @@
+import MainPage from "./main";
+import DetailsPage from "./details";
+import LoginPage from "./login";
+import SignupPage from "./signup";
+import NotFoundPage from "./not-found";
+import PaymentPage from "./payment";
+import SearchPage from "./search";
+import SearchResultPage from "./search-result";
+import UserInfoPage from "./user-info";
+import WishlistPage from "./wishlist";
+
+export  { 
+  DetailsPage,
+  LoginPage,
+  MainPage,
+  NotFoundPage,
+  PaymentPage,
+  SearchPage,
+  SearchResultPage,
+  SignupPage,  
+  UserInfoPage,
+  WishlistPage
+}
---
+++ src/pages/signup/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SignupPage = () =>  (
+  <div>Signup Page</div>
+)
+
+export default SignupPage
