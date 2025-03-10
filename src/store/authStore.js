7980b06e9497b942f1f6e156e498a7366b045f5e
jadewisemann <jadewisemann@gmail.com>
2025-03-10 10:44:49 +0900

    feat: add auth store

---
+++ src/store/authStore.js
@@ -0,0 +1,38 @@
+// firebase와의 의존성 분리
+
+import { create } from "zustand";
+import { listenAuthState, signUp, login, logout } from "../firebase/auth";
+
+const useAuthStore = create((set) => {
+  listenAuthState((user) => set({ user }));
+
+  return {
+    user: null,
+    error: null,
+
+    signUp: async (email, password) => {
+      try {
+        const userCredential = await signUp(email, password);
+        set({ user: userCredential.user, error: null });
+      } catch (error) {
+        set({ error: error.message });
+      }
+    },
+
+    login: async (email, password) => {
+      try {
+        const userCredential = await login(email, password);
+        set({ user: userCredential.user, error: null });
+      } catch (error) {
+        set({ error: error.message });
+      }
+    },
+
+    logout: async () => {
+      await logout();
+      set({ user: null });
+    },
+  };
+});
+
+export default useAuthStore;
