b11bfbba33f4f32786bf36a7f5871812a57715ac
jadewisemann <jadewisemann@gmail.com>
2025-03-11 16:30:51 +0900

    feat: add test page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,16 @@
-import MainPage from "./main";
-import DetailsPage from "./details";
-import LoginPage from "./login";
-import SignupPage from "./signup";
-import NotFoundPage from "./not-found";
-import PaymentPage from "./payment";
-import SearchPage from "./search";
-import SearchResultPage from "./search-result";
-import UserInfoPage from "./user-info";
-import WishlistPage from "./wishlist";
-import TestPage from "./test";
+import MainPage from './main';
+import DetailsPage from './details';
+import LoginPage from './login';
+import SignupPage from './signup';
+import NotFoundPage from './not-found';
+import PaymentPage from './payment';
+import SearchPage from './search';
+import SearchResultPage from './search-result';
+import UserInfoPage from './user-info';
+import WishlistPage from './wishlist';
+import TestPage from './@test';
 
-export  { 
+export {
   DetailsPage,
   LoginPage,
   MainPage,
@@ -18,8 +18,8 @@ export  {
   PaymentPage,
   SearchPage,
   SearchResultPage,
-  SignupPage,  
+  SignupPage,
   UserInfoPage,
   WishlistPage,
-  TestPage
-}
+  TestPage,
+};
