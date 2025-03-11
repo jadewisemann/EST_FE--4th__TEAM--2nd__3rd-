f60c5bf5274cd9c5c377a7bf51a32e86ac1a934d
jadewisemann <jadewisemann@gmail.com>
2025-03-11 11:02:07 +0900

    chore: add eslint setting

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,66 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
+// import tailwindcss from 'eslint-plugin-tailwindcss'
 
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+export default {
+  ignores: ['dist'],
+  files: ['**/*.{js,jsx}'],
+
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: {
+      ...globals.browser,
+      React: 'readonly',
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-]
+
+  settings: {
+    react: {
+      runtime: 'automatic',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+      },
+    },
+  },
+
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    'prettier': prettier,
+    'react': react,
+    'jsx-a11y': jsxA11y,
+    'import': importPlugin,
+    // 'tailwindcss': tailwindcss
+  },
+
+  rules: {
+    ...js.configs.recommended.rules,
+    ...reactHooks.configs.recommended.rules,
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'warn',
+    'prefer-arrow-callback': 'warn',
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': 'error',
+    'eqeqeq': ['error', 'always'],
+    'func-style': ['warn', 'expression'],
+    // 'tailwindcss/classnames-order': 'warn',    
+    // 'tailwindcss/no-custom-classname': 'off'
+  },
+}
