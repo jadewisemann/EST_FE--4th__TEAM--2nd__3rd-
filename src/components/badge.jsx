2dc7c7046ad90739dcd334e642001aa6a3003187
kib09 <dslqoehf@gmail.com>
2025-03-13 13:45:59 +0900

    fix: badge componet
    뱃지 컴포넌트 선언 방식 수정 및 주석 추가

--- src/components/badge.jsx
+++ src/components/badge.jsx
@@ -1,5 +1,4 @@
-const Badge = () => {
-  const sale = 10;
+export default function Badge({ sale }) {
   return (
     <>
       <div className='inline-flex items-center justify-center gap-2.5 rounded-xs bg-violet-200 p-0.5 text-sm'>
@@ -9,6 +8,9 @@ const Badge = () => {
       </div>
     </>
   );
-};
+}
 
-export default Badge;
+// 사용법
+// 예시 <Badge sale={rooms.sale} />
+// 컴포넌트 선언후 추후 db에 들어있는 rooms.sale을 넣어주면 됩니다
+//
