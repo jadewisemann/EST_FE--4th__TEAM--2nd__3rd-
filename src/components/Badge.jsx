5eccc20ba323b707ad0d33c8f0a6c10a5055a843
kib09 <dslqoehf@gmail.com>
2025-03-14 13:20:09 +0900

    fix : badge component
    코드 반복 최소화

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,26 +1,12 @@
-const Badge = ({ children, type }) => {
-  if (type === 'sale') {
-    return (
-      <div className='inline-flex items-center justify-center gap-2.5 rounded-xs bg-violet-200 p-0.5 text-sm'>
-        <span className='font-inter text-sm font-medium text-violet-600'>
-          {children}% off
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className='inline-flex items-center justify-center gap-2.5 rounded-xs bg-violet-200 p-0.5 text-sm'>
-        <span className='font-inter text-sm font-medium text-violet-600'>
-          {children}
-        </span>
-      </div>
-    );
-  }
-};
+const Badge = ({ children, type }) => (
+  <div className='inline-flex items-center justify-center rounded-xs bg-violet-200 p-1 text-sm'>
+    <span className='font-inter text-sm font-medium text-violet-600'>
+      {type === 'sale' ? `${children}% off` : children}
+    </span>
+  </div>
+);
 
 export default Badge;
 // 사용법
-// 세일 태그  <Badge type={sale} chiledren={}  /> -- 추후db에 sale 값 가져오기
+// 세일 태그  <Badge type={sale} chiledren={15}  /> children -- 추후db에 sale 값 가져오기
 // 텍스트 태그  <Badge chiledren={오늘의 최저가} />
-// 컴포넌트 선언후 추후 db에 들어있는 rooms.sale을 넣어주면 됩니다
-//
