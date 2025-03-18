f2cc988675f51e07052b193e275605fcc2f549d5
green5 <a1004712@naver.com>
2025-03-18 16:15:34 +0900

    fix: Structural change..

--- src/components/DetailSection.jsx
+++ src/components/DetailSection.jsx
@@ -3,36 +3,11 @@ const DetailSection = ({
   title,
   type,
   color = 'text-neutral-600',
-  size = 'sm',
+  size = 'text-sm',
   weight = 'font-normal',
   contents = [],
 }) => {
   const cx = (...classes) => classes.filter(Boolean).join(' ');
-  const sizes = {
-    xxs: 'text-xxs',
-    xs: 'text-xs',
-    sm: 'text-sm',
-    md: 'text-base',
-    lg: 'text-lg',
-    xl: 'text-xl',
-    xxl: 'text-2xl',
-    xxxl: 'text-3xl',
-  };
-  const colors = {
-    neutral300: 'text-neutral-300',
-    neutral400: 'text-neutral-400',
-    neutral500: 'text-neutral-500',
-    neutral600: 'text-neutral-600',
-    black: 'text-black',
-    white: 'text-white',
-    violet: 'text-violet-600',
-    red: 'text-red-500',
-  };
-  const weights = {
-    bold: 'font-bold',
-    normal: 'font-normal',
-  };
-  const fontStyleClasses = cx(sizes[size], colors[color], weights[weight]);
 
   return (
     <>
@@ -41,15 +16,22 @@ const DetailSection = ({
         <div>
           {type === 'list-left' &&
             contents.map((item, idx) => (
-              <ul key={idx} className={fontStyleClasses}>
+              <ul key={idx} className={`${size} ${color} ${weight}`}>
                 <li className='py-0.5'>{item}</li>
               </ul>
             ))}
 
           {type === 'list-left-dot' &&
             contents.map((item, idx) => (
-              <ul key={idx} className={`list-disc ${fontStyleClasses}`}>
-                <li className='ml-5 py-0.5'>{item}</li>
+              <ul
+                key={idx}
+                className={`ml-5 list-disc ${size} ${color} ${weight}`}
+              >
+                <li
+                  className={`py-0.5 ${item.isHighlighted ? 'font-bold text-red-500' : ''}`}
+                >
+                  {item.text}
+                </li>
               </ul>
             ))}
 
@@ -57,7 +39,10 @@ const DetailSection = ({
             contents.map((item, idx) => (
               <div key={idx}>
                 <h4 className='my-1 text-sm font-bold'>{item.subTitle}</h4>
-                <ul className={`mb-2.5 list-disc ${fontStyleClasses}`}>
+                <ul
+                  key={idx}
+                  className={`mb-2.5 list-disc ${size} ${color} ${weight}`}
+                >
                   {item.subContents.map((content, i) => (
                     <li key={i} className='ml-5 py-0.5'>
                       {content}
@@ -71,7 +56,7 @@ const DetailSection = ({
             contents.map((item, idx) => (
               <div
                 key={idx}
-                className={`flex flex-col gap-1.5 ${fontStyleClasses}`}
+                className={`flex flex-col gap-1.5 ${size} ${color} ${weight}`}
               >
                 {Object.entries(item.labels).map(([key, label]) => (
                   <div key={key} className='flex items-start'>
@@ -99,20 +84,15 @@ const DetailSection = ({
                   className='flex items-center justify-between text-sm font-normal text-neutral-500'
                 >
                   <div
-                    className={`${item.label === '최종 결제금액' ? 'text-base text-black' : null}`}
+                    className={`${item.label === '최종 결제금액' ? 'text-base text-black' : ''}`}
                   >
                     {item.label}
                   </div>
                   <div
-                    className={`${item.label === '최종 결제금액' ? 'text-lg font-bold text-violet-600' : null}`}
+                    className={`${item.label === '최종 결제금액' ? 'text-lg font-bold text-violet-600' : ''}`}
                   >
                     {item.showMore ? (
-                      <button
-                        className='cursor-pointer'
-                        // onClick={onMoreClick(item.label)}
-                      >
-                        더보기 &gt;
-                      </button>
+                      <button className='cursor-pointer'>더보기 &gt;</button>
                     ) : (
                       item.value
                     )}
