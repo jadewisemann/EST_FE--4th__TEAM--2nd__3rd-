a0815e49d1d68acd5cef5bc9bd9b56ed4bb38336
HANEUNHYUCK <changehan123@gmail.com>
2025-03-18 13:48:12 +0900

    feat: add reservation detail page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ import SearchPage from './search';
 import StayListpage from './search-result';
 import UserInfoPage from './user-info';
 import WishlistPage from './wishlist';
+import ReservationDetailPage from './reservation-detail';
 import TestPage from './@test';
 
 export {
@@ -23,5 +24,6 @@ export {
   SignupPage,
   UserInfoPage,
   WishlistPage,
+  ReservationDetailPage,
   TestPage,
 };
