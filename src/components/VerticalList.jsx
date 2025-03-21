70af9c1b9229298bd903f1b5a97cd89fd54217a4
green5 <a1004712@naver.com>
2025-03-21 15:29:15 +0900

    fix: Change the data transfer method

--- src/components/VerticalList.jsx
+++ src/components/VerticalList.jsx
@@ -4,44 +4,53 @@ import Icon from '../components/Icon';
 
 const VerticalList = ({ products }) => (
   <>
-    <div></div>
     {products?.map((product, index) => (
-      <div key={index} className='border-b-1 border-neutral-200 py-4'>
+      <button
+        key={index}
+        className='w-full cursor-pointer border-b-1 border-neutral-200 py-4'
+        onClick={() => {
+          alert('확인');
+        }}
+      >
         <div type='button' className='flex w-full gap-3'>
-          <button className='cursor-pointer'>
+          <div className='shrink-0'>
             <img
-              className='block h-20 w-20 rounded-[10px]'
-              src={product.thumbnail}
+              className='block h-20 w-20 rounded-[10px] object-cover'
+              src={product.rooms?.[0]?.img || product.image?.[0] || ''}
               alt=''
             />
-          </button>
+          </div>
           <div className='flex grow justify-between'>
             <div className='flex flex-col'>
-              <button className='cursor-pointer'>
-                <h2 className='mb-1.5 text-left text-sm'>{product.name}</h2>
-              </button>
+              <div className=''>
+                <h2 className='mb-1.5 text-left text-sm'>{product.title}</h2>
+              </div>
               <address className='flex items-center text-[11px] text-neutral-500 not-italic'>
                 <Icon name='location' className='text-neutral-500' size={20} />
-                {product.location}
+                {product.location[0]}
               </address>
               <div className='mt-1 mt-auto flex items-end gap-1'>
                 <span className='text-sm font-bold text-violet-600'>
-                  {product.price.toLocaleString()}원
+                  {(product.rooms?.[0]?.price ?? 0).toLocaleString()}원
                 </span>
                 <span className='text-xs text-neutral-500'>/ 1박</span>
               </div>
             </div>
             <div className='flex flex-col justify-between'>
               <div className='ml-auto'>
-                <Rating rate={product.rate} />
+                <Rating rate={product._debug?.score || 0} />
               </div>
-              <div className=''>
-                <Heart />
+              <div
+                onClick={e => {
+                  e.stopPropagation(); // 여기서 버블링 막기
+                }}
+              >
+                <Heart className='relative z-10' />
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </button>
     ))}
   </>
 );
