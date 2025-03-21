5820b63f05b18b5e974aaa6f2d604d062049fee4
HANEUNHYUCK <changehan123@gmail.com>
2025-03-21 13:20:09 +0900

    fix : add onchange event

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,18 +1,33 @@
-const CheckBox = ({ name, txt, disabled, className }) => (
-  <div className={`flex items-center gap-1 ${className || ''}`}>
-    <input
-      id={name}
-      type='checkbox'
-      disabled={disabled}
-      className='peer aspect-square w-[0.875rem] accent-violet-600'
-    />
-    <label
-      htmlFor={name}
-      className={`text-sm font-medium peer-checked:text-black ${disabled ? 'text-neutral-400' : ''}`}
-    >
-      {txt}
-    </label>
-  </div>
-);
+import { useId } from 'react';
+
+const CheckBox = ({
+  txt,
+  disabled = false,
+  checked = false,
+  className = '',
+  onChange,
+}) => {
+  const inputId = useId();
+
+  return (
+    <div className={`flex items-center gap-1 ${className}`}>
+      <input
+        id={inputId}
+        type='checkbox'
+        disabled={disabled}
+        aria-disabled={disabled}
+        className='peer aspect-square w-[0.875rem] accent-violet-600'
+        defaultChecked={checked}
+        onChange={onChange}
+      />
+      <label
+        htmlFor={inputId}
+        className={`text-sm font-medium peer-checked:text-black ${disabled ? 'text-neutral-400' : ''}`}
+      >
+        {txt}
+      </label>
+    </div>
+  );
+};
 
 export default CheckBox;
