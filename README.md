f0db263528f9c398d885ab8909f3789aee960c67
kib09 <dslqoehf@gmail.com>
2025-04-03 09:42:20 +0900

    feat: add animation to toast messages
    토스트 메세지에 애니메이션을 추가했습니다 .

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - component - checkbox, icon, radio, rating, verticallist
 - page - details, checkout, reservation-detail, wishlist
 - 
+
+- 김인배 한 일 
+- component - input , toast , skeleton , heart , loading 
+- page - login , signup , find-password 
+- routes - PrivateRoute , PublicRoute
--- src/components/ToastProvider.jsx
+++ src/components/ToastProvider.jsx
@@ -3,12 +3,10 @@ import useToastStore from '../store/toastStore';
 const ToastProvider = () => {
   const { message, className } = useToastStore();
 
-  if (!message) return null;
-
   return (
     <div
       role='alert'
-      className={`fixed top-10 left-1/2 z-[1000] -translate-x-1/2 rounded-lg bg-gray-800 px-4 py-2 text-white shadow-md ${className}`}
+      className={`fixed top-10 left-1/2 z-[1000] -translate-x-1/2 rounded-2xl bg-gray-800 px-4 py-3 text-white shadow-md transition-transform duration-500 ${message ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} ${className}`}
     >
       {message}
     </div>
